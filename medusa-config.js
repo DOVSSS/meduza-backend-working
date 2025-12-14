b71be42debf0e643eb11ab87af43e033e18c1221
Isa Abdurzacov <mutafailu@gmail.com>
2025-12-14 12:49:22 +0300

    F error

--- medusa-config.js
+++ medusa-config.js
@@ -1,12 +1,30 @@
-module.exports = {
+export default {
   projectConfig: {
-    port: 9000,
+    
+    port: parseInt(process.env.PORT || 9000, 10),
+    
+    
     database_url: "sqlite:///medusa.db",
     database_type: "sqlite",
-    jwt_secret: "test123",
-    cookie_secret: "test123",
+    
+    
+    http: {
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    
+    
     store_cors: "*",
     admin_cors: "*",
+    auth_cors: "*",
   },
+  
   plugins: [],
+  
+  modules: {},
+  
+  featureFlags: {
+
+    admin: false,
+  }
 };
